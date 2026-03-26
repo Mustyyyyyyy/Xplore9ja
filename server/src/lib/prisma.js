18f3d98208c668675eb29ddcd35eae531227a3c7
Mustapha Alagbe <alagbemustapha02@gmail.com>
2026-03-26 10:39:14 -0700

    W

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,10 +1,12 @@
 const { PrismaClient } = require("@prisma/client");
+const { PrismaPg } = require("@prisma/adapter-pg");
 
 const globalForPrisma = global;
 
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL }),
     log: ["warn", "error"],
   });
 
