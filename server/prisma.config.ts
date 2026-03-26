a74eb53f1bb4275d68b70c44fedebb0ec80261b9
Mustapha Alagbe <alagbemustapha02@gmail.com>
2026-03-26 11:25:27 -0700

    W

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,8 +1,6 @@
-import type { PrismaConfig } from "prisma";
-
 export default {
   schema: "./prisma/schema.prisma",
   datasource: {
     url: process.env.DATABASE_URL,
   },
-} satisfies PrismaConfig;
+};
